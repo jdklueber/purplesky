e4ee37dbb94306b135f4349e43ab36e0c7e75f7f
jdklueber <jklueber@thesoftwareguild.com>
2019-10-14 22:15:39 -0400

    Initial commit

---
+++ src/test/java/com/jdklueber/underpurpleskies/AppTests.java
@@ -0,0 +1,16 @@
+package com.jdklueber.underpurpleskies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
